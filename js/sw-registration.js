b04d89b7e2c62e89f906107fcb1db859015b1eac
CodewithJW <dlutbjw@gmail.com>
2024-05-05 00:54:28 +0900

    update

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -6,7 +6,7 @@
  * Register service worker.
  * ========================================================== */
 
-// SW Version Upgrade Ref: <https://youtu.be/Gb9uI67tqV0>
+// SW Version Upgrade
 
 function handleRegistration(registration){
   console.log('Service Worker Registered. ', registration)
@@ -35,7 +35,7 @@ if(navigator.serviceWorker){
   // For security reasons, a service worker can only control the pages
   // that are in the same directory level or below it. That's why we put sw.js at ROOT level.
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('/js/sw.js')
     .then((registration) => handleRegistration(registration))
     .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
 
